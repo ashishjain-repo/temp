7b4fcaaf61ed13360d970dea9d1bf211dfa61160
Ashish Jain <ashishjain99@yahoo.com>
2024-01-27 08:39:24 -0700

    made changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-const linksList = ["Home","Jewellery","Guests","Plans","License","Gifts Registery","Decoration","Food","Invitations","Photoshoot","Venue","Playlist","Wedding Album"]
+function anchorShow(linksList)
+{
+    let linksContainer = document.getElementById("linksContainer")
+    for(let i = 0; i < linksList.length; i++)
+    {
+        let aElement = document.createElement("a");
+        aElement.classList.add("navLinks");
+        aElement.value = linksList[i];
+        linksContainer.appendChild(aElement);
+    }
+}
+
+let linksList = ["Home","Jewellery","Guests","Plans","License","Gifts Registery","Decoration","Food","Invitations","Photoshoot","Venue","Playlist","Wedding Album"];
+
+anchorShow(linksList);
