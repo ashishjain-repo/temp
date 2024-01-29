d6c2c82caedaac0c85a5bae1ff4703775955e1ec
Ashish Jain <ashishjain99@yahoo.com>
2024-01-28 17:21:34 -0700

    New changes

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./styles/main.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+<nav class="navbar">
+    <div class="nav-list">
+        <a href="" class="nav-list-links">Home</a>
+        <a href="" class="nav-list-links">Location</a>
+        <a href="" class="nav-list-links">Registry</a>
+        <a href="" class="nav-list-links">Photos</a>
+    </div>
+</nav>
+</header>
+
+<main>
+    <div class="main-names">
+<p>Ashish & Skya</p>
+        <p>Are Getting Married</p>
+
+    </div>
+    <div class="main-dates">
+<p>Save The Date</p>
+        <p>April.13.2024</p>
+    </div>
+    <div class="main-venue">
+<p>3301 S Greenfield Rd <br> Gilbert AZ 85297 <br> United States</p>
+    </div>
+    <div class="main-rsvp">
+
+    </div>
+</main>
+<footer>
+    <div>
+<p class="footer-line">Two are better than one, because they have a good reward for their toil.</p>
+    </div>
+</footer>
+</body>
+</html>
